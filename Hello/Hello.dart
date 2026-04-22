48968980bf51cca6adb34f5969bc91560817cd14
Rony7s <bdu.rony@gmail.com>
2026-04-22 20:21:23 +0600

    Add Hello.dart file

---
+++ Hello/Hello.dart
@@ -0,0 +1,17 @@
+//v1.0.0
+void main() {
+  print("Hello, World!");
+
+  var name = "Rony Ahmmed";
+  var age = 24;
+  print("My name is $name and age $age");
+
+  int? roll = 2002035;
+  String? dept;
+  dept = "EdTE";
+  print("My department name is $dept and \nstudent id is ${2002000 + roll}");
+
+  List<String> sub = ["C proggramming", "Java", "Dart"];
+  print(sub);
+  print("I love ${sub[1]}");
+}
